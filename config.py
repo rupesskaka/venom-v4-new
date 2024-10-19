3576e28e9fac06c44ed7a7c1e818f2a0b0239965
rupesskaka <kingindonisia2@gmail.com>
2024-10-19 14:30:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7828802090:AAF0pfyIBCP02q327BTcqctiL6GvIHBgIRA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("TC sanatan")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7186024164")
 
 
 
